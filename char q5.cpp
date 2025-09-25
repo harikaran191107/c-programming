c7c4ea6fb2c5a14d042470dc42c819ca440d698c
harikaran191107 <harikaran191107@gmail.com>
2025-09-25 14:38:12 +0530

    Add files via upload

---
+++ char q5.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	char a,b;
+	scanf("%c %c",&a,&b);
+	if(a==b)
+	printf("the given two characters are equal");
+	else
+	printf("the given two characters are not equal");
+return 0;
+}
